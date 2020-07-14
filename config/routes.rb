5f0ae42afb7deda94caf4d8df624c5cf145b9d37
MMcClure11 <mcclme02@luther.edu>
2020-07-14 09:51:07 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'authors/show'
+  # get 'authors/show'
+
 
   get 'test/index'
+
+  resources :authors, only: [:show]
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
